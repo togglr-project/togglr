9015c9c7efce420b31705fd043ff7bf9bee217a3
Roman Chudov <roman.chudov@gmail.com>
2025-09-12 10:38:39 +0300

    ping-pong handler.

--- internal/api/rest/api.go
+++ internal/api/rest/api.go
@@ -1,14 +1,21 @@
 package rest
 
 import (
+	"context"
+
 	generatedapi "github.com/rom8726/etoggl/internal/generated/server"
 )
 
-var _ (generatedapi.Handler) = (*RestAPI)(nil) // TODO: implement!
+var _ (generatedapi.Handler) = (*RestAPI)(nil)
 
 type RestAPI struct {
 }
 
 func New() *RestAPI {
 	return &RestAPI{}
+}
+
+func (r RestAPI) Ping(ctx context.Context) (generatedapi.PingOK, error) {
+	//TODO implement me
+	panic("implement me")
 }
