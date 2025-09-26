5784fc0bf4b4125196bb9a204185ad7292410e0b
Roman Chudov <roman.chudov@gmail.com>
2025-09-26 20:31:11 +0300

    environments.

---
+++ migrations/029_environments.up.sql
@@ -0,0 +1,107 @@
+CREATE TABLE environments (
+    id BIGSERIAL PRIMARY KEY,
+    project_id UUID NOT NULL REFERENCES projects(id) ON DELETE CASCADE,
+    key VARCHAR(20) NOT NULL, -- dev, stage, prod
+    name VARCHAR(50) NOT NULL,
+    api_key UUID NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    UNIQUE (project_id, key)
+);
+
+ALTER TABLE projects DROP COLUMN api_key; -- in environments for a project now.
+
+CREATE TABLE feature_params (
+    feature_id UUID NOT NULL REFERENCES features(id) ON DELETE CASCADE,
+    environment_id BIGINT NOT NULL REFERENCES environments(id) ON DELETE CASCADE,
+    enabled BOOLEAN NOT NULL DEFAULT false,
+    default_value VARCHAR(128),
+    created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+    PRIMARY KEY (feature_id, environment_id)
+);
+
+CREATE OR REPLACE FUNCTION update_feature_params_modified()
+    RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_feature_params_updated
+    BEFORE UPDATE ON feature_params
+    FOR EACH ROW
+EXECUTE FUNCTION update_feature_params_modified();
+
+ALTER TABLE features DROP COLUMN enabled;
+ALTER TABLE features DROP COLUMN default_variant;
+
+ALTER TABLE rules ADD COLUMN environment_id BIGINT NOT NULL default 0;
+
+ALTER TABLE rules
+    ADD CONSTRAINT rules_environment_id_fkey
+        FOREIGN KEY (environment_id) REFERENCES environments(id) ON DELETE CASCADE;
+
+ALTER TABLE rules
+    DROP CONSTRAINT IF EXISTS rules_unique_priority;
+
+ALTER TABLE rules
+    ADD CONSTRAINT rules_unique_priority_per_env
+        UNIQUE (feature_id, environment_id, priority);
+
+ALTER TABLE flag_variants ADD COLUMN environment_id BIGINT NOT NULL DEFAULT 0;
+
+ALTER TABLE flag_variants
+    ADD CONSTRAINT flag_variants_environment_id_fkey
+        FOREIGN KEY (environment_id) REFERENCES environments(id) ON DELETE CASCADE;
+
+ALTER TABLE flag_variants
+    DROP CONSTRAINT flag_variants_unique;
+
+ALTER TABLE flag_variants
+    ADD CONSTRAINT flag_variants_unique
+        UNIQUE (feature_id, environment_id, name);
+
+ALTER TABLE feature_schedules ADD COLUMN environment_id BIGINT NOT NULL DEFAULT 0;
+
+ALTER TABLE feature_schedules
+    ADD CONSTRAINT feature_schedules_environment_id_fkey
+        FOREIGN KEY (environment_id) REFERENCES environments(id) ON DELETE CASCADE;
+
+DROP INDEX IF EXISTS idx_feature_schedules_feature_id;
+CREATE INDEX idx_feature_schedules_feature_id ON feature_schedules(feature_id, environment_id);
+
+ALTER TABLE feature_schedules
+    DROP CONSTRAINT feature_schedules_no_overlap_guard;
+
+ALTER TABLE feature_schedules
+    ADD CONSTRAINT feature_schedules_no_overlap_guard
+        EXCLUDE USING gist (
+        feature_id WITH =,
+        environment_id WITH =,
+        tstzrange(starts_at, ends_at, '[]'::text) WITH &&
+    ) WHERE ((cron_expr IS NULL));
+
+-- ALTER TABLE segments ADD COLUMN environment_id BIGINT;
+-- ALTER TABLE segments ALTER COLUMN environment_id SET NOT NULL;
+-- ALTER TABLE segments
+--     ADD CONSTRAINT segments_environment_id_fkey
+--         FOREIGN KEY (environment_id) REFERENCES environments(id) ON DELETE CASCADE;
+-- ALTER TABLE segments
+--     DROP CONSTRAINT segments_project_id_name_key;
+-- ALTER TABLE segments
+--     ADD CONSTRAINT segments_project_env_name_key
+--         UNIQUE (project_id, environment_id, name);
+
+ALTER TABLE audit_log ADD COLUMN environment_id BIGINT;
+ALTER TABLE audit_log ALTER COLUMN environment_id SET NOT NULL;
+
+ALTER TABLE audit_log
+    ADD CONSTRAINT audit_log_environment_id_fkey
+        FOREIGN KEY (environment_id) REFERENCES environments(id) ON DELETE CASCADE;
+
+ALTER TABLE pending_changes ADD COLUMN environment_id BIGINT NOT NULL DEFAULT 0;
+
+ALTER TABLE pending_changes
+    ADD CONSTRAINT pending_changes_environment_id_fkey
+        FOREIGN KEY (environment_id) REFERENCES environments(id) ON DELETE CASCADE;
