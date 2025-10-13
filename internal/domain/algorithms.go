733304c74cee241d6fd7dec8d53aa62c3ae32490
Roman Chudov <roman.chudov@gmail.com>
2025-10-13 20:51:32 +0300

    domain.Algorithm structure.

--- internal/domain/algorithms.go
+++ internal/domain/algorithms.go
@@ -1,3 +1,35 @@
 package domain
 
+import (
+	"time"
+
+	"github.com/shopspring/decimal"
+)
+
 type AlgorithmID string
+
+type AlgorithmType uint8
+
+const (
+	AlgorithmTypeUnknown AlgorithmType = iota
+	AlgorithmTypeEpsilonGreedy
+	AlgorithmTypeThompsonSampling
+	AlgorithmTypeUCB
+)
+
+type AlgorithmKind string
+
+const (
+	AlgorithmKindBandit AlgorithmKind = "bandit"
+)
+
+type Algorithm struct {
+	ID              AlgorithmID
+	Name            string
+	Slug            AlgorithmType
+	Kind            AlgorithmKind
+	Description     string
+	DefaultSettings map[string]decimal.Decimal
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
+}
